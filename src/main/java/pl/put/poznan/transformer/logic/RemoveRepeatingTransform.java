c90b99cea1e6a81d933dd13d5bcbc62be6b03bd1
Onufry <onufryklaczynski@gmail.com>
2020-12-20 16:29:36 +0100

    #7 Eliminating repeating words

---
+++ src/main/java/pl/put/poznan/transformer/logic/RemoveRepeatingTransform.java
@@ -0,0 +1,26 @@
+package pl.put.poznan.transformer.logic;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class RemoveRepeatingTransform implements TextTransformerInterface {
+    @Override
+    public String transform(String text) {
+        String[] split = text.split("\\s");
+
+        for (int i = 1, splitLength = split.length; i < splitLength; i++) {
+            String lastWord = split[i];
+            String word = split[i - 1];
+            if (lastWord.equals(word)) {
+                split[i-1] = null;
+            }
+        }
+
+        return Arrays.stream(split)
+                .filter(Predicate.not(Objects::isNull))
+                .collect(Collectors.joining(" "));
+    }
+}
