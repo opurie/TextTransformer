a6777d580dd9b5a2fe6cb88d77c79881388cf3ae
marek <marek.zieba@icloud.com>
2020-12-21 17:35:00 +0100

    added javadoc

--- src/main/java/pl/put/poznan/transformer/rest/WebController.java
+++ src/main/java/pl/put/poznan/transformer/rest/WebController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Web controller for serving the GUI
+ */
 @Controller
 public class WebController {
+    /**
+     * Method for handling incoming get requests to /gui
+     * @return name of file containg the GUI
+     */
     @RequestMapping("/gui")
     public String web(){
         return "index.html";
